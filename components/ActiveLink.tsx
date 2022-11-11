3b29b472455bd8eb1d60e217ae9b0af5698598ad
Jorge Zamora <jorge.zamora60@hotmail.com>
2022-11-10 20:16:26 -0600

    Pre-deployment Vercel

---
+++ components/ActiveLink.tsx
@@ -0,0 +1,25 @@
+import { CSSProperties } from "react";
+
+import { useRouter } from "next/router";
+
+import Link from "next/link";
+
+const style: CSSProperties = {
+  color: "#0070f3",
+  textDecoration: "underline",
+};
+
+interface Props {
+  text: string;
+  href: string;
+}
+
+export const ActiveLink: React.FC<Props> = ({ text, href }) => {
+  const { asPath } = useRouter();
+
+  return (
+    <Link legacyBehavior href={href}>
+      <a style={asPath === href ? style : undefined}> {text} </a>
+    </Link>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import "../styles/globals.css";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode; //para no importar "ReactElement y ReactNode" podemos poner JSX.Element
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
